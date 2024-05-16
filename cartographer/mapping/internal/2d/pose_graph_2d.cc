ba06f6d702b5c8c047d1348985e7e6e66e570849
xiay <9046831+slam-xiay@user.noreply.gitee.com>
2024-05-16 16:45:39 +0800

    modify cartographer

--- cartographer/mapping/internal/2d/pose_graph_2d.cc
+++ cartographer/mapping/internal/2d/pose_graph_2d.cc
@@ -986,15 +986,15 @@ MapById<NodeId, TrajectoryNodePose> PoseGraph2D::GetNodePosesBySubmapId(
     const SubmapId& submap_id) const {
   MapById<NodeId, TrajectoryNodePose> node_poses;
   for (auto&& node_data : data_.submap_data.at(submap_id).node_ids) {
-    auto node_data = data_.trajectory_nodes.at(node_data.id);
+    // auto node_data = data_.trajectory_nodes.at(node_data.id);
     std::optional<TrajectoryNodePose::ConstantPoseData> constant_pose_data;
     // if (node_id_data.data.constant_data != nullptr) {
     //   constant_pose_data = TrajectoryNodePose::ConstantPoseData{
     //       node_id_data.data.constant_data->time,
     //       node_id_data.data.constant_data->local_pose};
     // }
-    node_poses.Insert(node_id.id, TrajectoryNodePose{node_data.global_pose,
-                                                     constant_pose_data});
+    node_poses.Insert(node_data.id, TrajectoryNodePose{node_data.global_pose,
+                                                       constant_pose_data});
   }
   return node_poses;
 }
